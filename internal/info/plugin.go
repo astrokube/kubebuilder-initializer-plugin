5731a4cb39ff57b2cfde6f4b221e2fa5692266a9
Iván Corrales <ivan.corrales.solera@gmail.com>
2023-04-02 09:43:38 +0200

    Add variables to be populated in the compilation time

---
+++ internal/info/plugin.go
@@ -0,0 +1,12 @@
+package info
+
+var (
+	Commit        string
+	Version       string
+	BuildDate     string
+	GolangVersion string
+)
+
+func Author() string {
+	return "The Astrokube Team <developer@astrokube.com>"
+}
--- pkg/info/plugin.go
+++
@@ -1,9 +0,0 @@
-package info
-
-func Author() string {
-	return "The Astrokube Team <developer@astrokube.com>"
-}
-
-func Version() string {
-	return "v1alpha"
-}
