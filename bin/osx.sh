e3da98daad02d8ff09252e37b4ffd3e7a6744d4a
Iván Corrales <ivan.corrales.solera@gmail.com>
2023-04-07 03:55:52 +0200

    PRovide an automatic mechanism to publish releases

--- bin/osx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/Library/Application\ Support/kubebuilder/plugins/kubebuilder-initializer/v1-alpha
-cp dist/kubebuilder-initializer-plugin ~/Library/Application\ Support/kubebuilder/plugins/kubebuilder-initializer/v1-alpha/kubebuilder-initializer
-
-
